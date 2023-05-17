5e0c54955e7b8f569fb883d828cd25b1bee43f72
Leticia Lima <leticiascferreira@gmail.com>
2023-05-17 19:07:07 -0300

    uso de DOM para receber as informações inseridas pelo usuário e aplicar na API

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json')
+/* var consultaCep = fetch('https://viacep.com.br/ws/01001000/json')
 .then(resposta => resposta.json())
 .then(r => {
     if (r.erro) {
@@ -9,10 +9,49 @@ var consultaCep = fetch('https://viacep.com.br/ws/01001000/json')
 })
 .catch(erro => console.log(erro))
 .finally(mensagem => console.log('Processo terminado'))
-console.log(consultaCep)
+console.log(consultaCep) */
 
-//json:
-//fetch:
-//then
-//catch:
-//finally: 
+//json: é um formato de dados baseado em texto seguindo a sintaxe do objeto JavaScript.
+//then: O método then() retorna uma Promise. Possui dois argumentos, ambos são "call back functions", sendo uma para o sucesso e outra para o fracasso da promessa.
+//catch: O método catch() retorna uma Promise e lida apenas com casos rejeitados.
+
+//Uso de asycn await para reescrever o código - syntax sugar.
+
+let cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
+
+async function buscaEndereco(cep) {
+		const mensagemErro = document.getElementById('erro');
+		mensagemErro.innerHTML = "";
+
+    try {
+        const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json`);
+        const consultaCepConvertida = await consultaCep.json();
+			
+				if (consultaCepConvertida.erro) {
+					throw Error('CEP não existente');
+				}
+
+				const cidade = document.getElementById('cidade');
+				const logradouro = document.getElementById('endereco');
+				const estado = document.getElementById('estado');
+
+				cidade.value = consultaCepConvertida.localidade;
+				logradouro.value = consultaCepConvertida.logradouro;
+				estado.value = consultaCepConvertida.uf;
+
+        console.log(consultaCepConvertida);
+				return consultaCepConvertida;
+
+    } catch (erro) {
+			console.log(erro);
+			mensagemErro.innerHTML = "CEP inválido, tente novamente."
+		}
+    
+}
+
+/*let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));*/
+//promise.all permite fazer várias requisições ao mesmo tempo.
+
